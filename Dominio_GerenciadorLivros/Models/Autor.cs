b914c2745f2a01e157cb8adb7ffc5c3687c11653
Cris Andrade <cristina.andrade@al.infnet.edu.br>
2020-12-11 12:19:53 -0300

    concluido!

--- Dominio_GerenciadorLivros/Models/Autor.cs
+++ Dominio_GerenciadorLivros/Models/Autor.cs
@@ -12,9 +12,11 @@ namespace Dominio_GerenciadorLivros.Models
         public string Email { get; set; }
         public DateTime DataNascimento { get; set; }
         public List<ID_Classes> Livros { get; set; }
+       
 
         public Autor()
         {
+            
             Livros = new List<ID_Classes>();
         }
     }
